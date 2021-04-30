13fb1e8f7ea7f6a149ee88411f50c95dd087fa38
Alan Chocano <56395076+AlanChocano@users.noreply.github.com>
2021-04-30 00:33:04 -0400

    Fusionamos cambios

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,6 @@
 use App\Http\Controllers\TrabajadorController;
 use Illuminate\Support\Facades\Route;
 
-<<<<<<< HEAD
 Route::get('/', function () {
     return view('admin.colaboradores.index');
 });
@@ -12,7 +11,6 @@ Route::get('/', function () {
 
 Route::get('/colaboradores/listado-colaboradores', [App\Http\Controllers\TrabajadorController::class, 'index']);
 Route::get('/colaboradores/axios/listado-colaboradores', [App\Http\Controllers\TrabajadorController::class, 'lista_trabajadores']);
-=======
 //TRABAJADORES ROUTING
 Route::get('/trabajador', [TrabajadorController::class, 'index'])->name('trabajador.index');
 Route::get('/trabajador/create', [TrabajadorController::class, 'create'])->name('trabajador.create');
@@ -21,4 +19,3 @@ Route::post('/trabajador',       [TrabajadorController::class, 'store'])->name('
 //UBICACION ROUTING
 Route::post('/trabajador/comunas',     [TrabajadorController::class, 'comunas']);
 Route::post('/trabajador/provincias',  [TrabajadorController::class,'provincias']);
->>>>>>> feature/Alan
